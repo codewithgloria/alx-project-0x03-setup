81429da8d4debeccf1dc903b59c21c10e67c2fc1
Gloria <abinezagloria@gmail.com>
2025-06-08 21:17:53 +0300

    completed task 2

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1 className="text-center text-4xl font-bold mt-20">Welcome to Splash App</h1>
-      <p className="text-center mt-4">Find your favorite place here!</p>
+      <h1>Welcome to Splash App</h1>
     </div>
   );
 };
