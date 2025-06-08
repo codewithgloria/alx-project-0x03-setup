0454b27ec03b97fa3d8aadf0e2686142753e1028
Gloria <abinezagloria@gmail.com>
2025-06-08 21:10:25 +0300

    initial commit

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-center text-4xl font-bold mt-20">Welcome to Splash App</h1>
+      <p className="text-center mt-4">Find your favorite place here!</p>
+    </div>
+  );
+};
+
+export default Home;
